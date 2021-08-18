33e25254d98d7573fdce41badda106b77dda85a4
Florin Mihalache <florin.mihalache@ionos.com>
2021-08-18 17:17:54 +0300

    cloning through https

--- clone.sh
+++ clone.sh
@@ -2,8 +2,9 @@
 
 set -uex
 
-git clone git@github.com:ionos-cloud/sdk-test-drivers --branch master --single-branch /tmp/test-drivers
-git clone git@github.com:ionos-cloud/${IONOS_SDK_NAME} /tmp/${IONOS_SDK_NAME}
+git clone https://${GITHUB_PAT}@github.com/ionos-cloud/sdk-test-drivers.git --branch master --single-branch /tmp/test-drivers
+
+git clone https://${GITHUB_PAT}@github.com/ionos-cloud/${IONOS_SDK_NAME} /tmp/${IONOS_SDK_NAME}
 
 # vim: ts=2: sw=2: ai: si
 
