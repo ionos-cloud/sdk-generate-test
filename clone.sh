1612491315a56014152aa8932d1ed8469475fb92
AnaMariaMoga <anamoga435@gmail.com>
2021-08-19 14:10:59 +0300

    feat: add sdk-branch

--- clone.sh
+++ clone.sh
@@ -4,7 +4,7 @@ set -uex
 
 git clone https://${GITHUB_PAT}@github.com/ionos-cloud/sdk-test-drivers.git --branch master --single-branch /tmp/test-drivers
 
-git clone https://${GITHUB_PAT}@github.com/ionos-cloud/${IONOS_SDK_NAME} /tmp/${IONOS_SDK_NAME}
+git clone https://${GITHUB_PAT}@github.com/ionos-cloud/${IONOS_SDK_NAME} --branch ${IONOS_SDK_BRANCH} /tmp/${IONOS_SDK_NAME}
 
 # vim: ts=2: sw=2: ai: si
 
