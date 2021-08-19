c1902f4bdd2756a13a310145521c259b53d39713
AnaMariaMoga <anamoga435@gmail.com>
2021-08-19 14:26:00 +0300

    fix: create branch

--- clone.sh
+++ clone.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-set -uex
+set -ux
 
 git clone https://${GITHUB_PAT}@github.com/ionos-cloud/sdk-test-drivers.git --branch master --single-branch /tmp/test-drivers
 
-git clone https://${GITHUB_PAT}@github.com/ionos-cloud/${IONOS_SDK_NAME} --branch ${IONOS_SDK_BRANCH} /tmp/${IONOS_SDK_NAME}
+# git clone https://${GITHUB_PAT}@github.com/ionos-cloud/${IONOS_SDK_NAME} /tmp/${IONOS_SDK_NAME}
+git clone https://${GITHUB_PAT}@github.com/ionos-cloud/${IONOS_SDK_NAME}
+
+pushd "${IONOS_SDK_NAME}"
+
+git fetch --prune --tags
+
+git branch -a | grep ${IONOS_SDK_BRANCH}
+if [ "$?" = "0" ]; then
+  git checkout ${IONOS_SDK_BRANCH} >/dev/null || exit 1
+else
+  info "creating a new branch: ${IONOS_SDK_BRANCH}"
+  git checkout -b ${IONOS_SDK_BRANCH} >/dev/null || exit 1
+  git push -u origin ${IONOS_SDK_BRANCH} >/dev/null || exit 1
+fi
+
+cp -rf . /tmp/${IONOS_SDK_NAME}
+
+popd
 
 # vim: ts=2: sw=2: ai: si
 
